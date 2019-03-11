10c6f4f80bb7917cba68016154f4f6b7ac4b9080
Tyler Corbett <thetylercorbett@gmail.com>
2019-03-11 15:46:10 -0700

    test passing

---
+++ src/components/App.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+export default function Home() {
+  return (
+    <>
+        <header>
+          <img src="https://proxy.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.kendavis.com%2Fwordpress%2Fwp-content%2Fuploads%2F2014%2F03%2Ftroll.jpg&f=1" alt='Internet troll'/>
+          <img src="https://proxy.duckduckgo.com/iu/?u=http%3A%2F%2Fthereddragoncentre.co.uk%2Fwp-content%2Fthemes%2Fburningred%2Fimages%2Ficons%2Ftwitter-red.png&f=1" alt='Squawk logo'/>
+        </header>
+        <main>
+          <article>
+            <img src='https://proxy.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.pankypost.com%2Fwp-content%2Fuploads%2F2018%2F06%2Ffattest-woman-angry-image-download-500x333.jpg&f=1' alt='Angry woman with rolling pin'/>
+            <p>Bone apple teeth!</p>
+            <img src='https://proxy.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.wereblog.com%2Fwp-content%2Fuploads%2F2014%2F10%2Fbattered-rat.jpg&f=1' alt='Deep fried rat'/>
+          </article>
+          <article>
+            <img src='https://proxy.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.kpopmusic.com%2Fwp-content%2Fuploads%2F2015%2F09%2Ft-o-p-profile-picture-759x500.jpg&f=1' alt='Korean man in glasses'/>
+            <p>Beyoncè had one of the greatest albums of ALL time!</p>
+          </article>
+          <img src='https://proxy.duckduckgo.com/iu/?u=http%3A%2F%2Fpngimages.net%2Fsites%2Fdefault%2Ffiles%2Fplus-png-image-68605.png&f=1' alt='Button to post a tweet'/>
+        </main>
+        <footer>
+          <nav>
+            <a href="/home">Home</a>
+            <a href="/search">Search</a>
+            <a href="/notifications">Notifications</a>
+            <a href="/messages">Messages</a>
+          </nav>
+        </footer>
+    </>
+  );
+}
+
+
---
+++ src/components/App.test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import App from './App';
+
+describe('App', () => {
+  it('matches a snapshot', () => {
+    const tree = renderer.create(
+      <App/>
+    ).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
